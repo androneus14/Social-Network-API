b2815455ae4069745669509caff4243e00f88df8
Andy Nguyen <andynguyen_3@hotmail.com>
2022-07-03 18:08:20 +1000

    added User model

---
+++ models/User.js
@@ -0,0 +1,49 @@
+const { Schema, model } = require('mongoose');
+
+const UserSchema = new Schema(
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: true,
+            trim: true
+        },
+
+        email: {
+            type: String,
+            unique: true,
+            required: true,
+            match: [/[\w._%+-]+@[\w.-]+\.[a-zA-z]{2,4}/, "Please enter a valid email",]
+        },
+
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'thought'
+            },
+        ],
+
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'User'
+            }
+        ]
+    },
+
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        id: false
+    }
+);
+
+UserSchema.virtual('friendCount').get(function () {
+    return this.friends.length;
+});
+
+const User = model('User', UserSchema);
+
+module.exports = User;
