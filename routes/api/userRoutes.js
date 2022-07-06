c1cb9f9f3c5ec6376b9fa3974d3d2204f691a30f
Andy Nguyen <andynguyen_3@hotmail.com>
2022-07-06 18:46:02 +1000

    added userRoutes. will update with friend routes after testing

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,1 +1,15 @@
 const router = require('express').Router();
+
+const{
+    getAllUsers,
+    getUserById,
+    newUser,
+    deleteUser,
+    updateUser
+} = require('../../controllers/userController');
+
+router.route('/').get(getAllUsers).post(newUser);
+
+router.route('/:userId').get(getUserById).put(updateUser).delete(deleteUser);
+
+module.exports = router;
